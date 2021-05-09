604b64899aaec211f7d2a47d454b21e43ddc08f6
Blackthe123 <71077658+Blackthe123@users.noreply.github.com>
2021-05-09 11:57:44 +0530

    Add files via upload

---
+++ slingShot.js
@@ -0,0 +1,34 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.04,
+            length:50
+        }
+        this.pointB = pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+
+    }
+    display(){
+     if(this.sling.bodyA){
+         strokeWeight(4);
+         push();
+         stroke("brown");
+         var pointA = this.sling.bodyA.position;
+         var pointB = this.pointB;
+         line(pointA.x, pointA.y, pointB.x, pointB.y);
+         pop();
+
+     }
+    }
+
+    fly(){
+     this.sling.bodyA = null;
+    }
+
+    attatch(body){
+     this.sling.bodyA = body;
+    }
+}
